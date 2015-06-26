c14042776ec398dd02dfd366f92c815766d07977
Max Lynch <max@drifty.com>
2015-06-26 12:03:20 -0500

    Body

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express'),
-    cors = require('cors');
+    cors = require('cors'),
+    bodyParser = require('body-parser');
 
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.json())
 app.use(cors({
   origin: function(origin, callback) {
     console.log('ORIGIN', origin);
@@ -13,24 +15,29 @@ app.use(cors({
   }
 }));
 
-function r(m) {
+function r(req) {
   return {
     'status': 'OK',
-    'method': 'get'
+    'method': req.method,
+    'data': req.body
   }
 }
 
+
 app.get('/json', function (req, res) {
-  res.json(r('get'));
+  res.json(r(req));
 });
 app.post('/json', function (req, res) {
-  res.json(r('post'));
+  res.json(r(req));
 });
 app.put('/json', function (req, res) {
-  res.json(r('put'));
+  res.json(r(req));
 });
 app.delete('/json', function (req, res) {
-  res.json(r('delete'));
+  res.json(r(req));
+});
+app.patch('/json', function (req, res) {
+  res.json(r(req));
 });
 
 app.listen(app.get('port'), function() {
